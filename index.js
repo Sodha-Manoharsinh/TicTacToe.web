a37f8e1045d30e0ce63807cfb2f73b9e85a88e87
Sodha Manoharsinh Chandubha <155339037+Sodha-Manoharsinh@users.noreply.github.com>
2024-05-04 19:43:30 +0530

    uploading files

---
+++ index.js
@@ -0,0 +1,75 @@
+const boxes = Array.from(document.getElementsByClassName("box"));
+const [button] = document.getElementsByTagName("button");
+const [heading] = document.getElementsByTagName("h1");
+
+const X_TURN = "X";
+const O_TURN = "O";
+
+let currentPlayer = Math.floor(Math.random() * 2) ? X_TURN : O_TURN;
+heading.innerHTML = `Turn = ${currentPlayer}`;
+
+let spaces = Array(9).fill(null);
+let counts_played = 0;
+
+button.addEventListener("click", restart);
+
+function startGame() {
+  for (let box of boxes) {
+    box.addEventListener("click", clicked);
+  }
+}
+
+function clicked(e) {
+  const id = e.target.id;
+  if (
+    !spaces[id] &&
+    counts_played != 9 &&
+    !checkWinner(spaces, currentPlayer)
+  ) {
+    spaces[id] = currentPlayer;
+    e.target.innerHTML = spaces[id];
+    if (checkWinner(spaces, currentPlayer)) {
+      boxes.forEach((box) => (box.style.backgroundColor = "green"));
+      heading.innerHTML = `Winner is ${currentPlayer}`;
+      return 0;
+    }
+    currentPlayer = currentPlayer == X_TURN ? O_TURN : X_TURN;
+    heading.innerHTML = `Turn = ${currentPlayer}`;
+    counts_played++;
+  }
+
+  if (counts_played == 9 && !checkWinner(spaces, currentPlayer)) {
+    heading.innerHTML = "DRAW";
+    boxes.forEach((box) => (box.style.backgroundColor = "red"));
+  }
+}
+
+function checkWinner(spaces, turn) {
+  if (
+    (spaces[0] == turn && spaces[1] == turn && spaces[2] == turn) ||
+    (spaces[3] == turn && spaces[4] == turn && spaces[5] == turn) ||
+    (spaces[6] == turn && spaces[7] == turn && spaces[8] == turn) ||
+    (spaces[0] == turn && spaces[3] == turn && spaces[6] == turn) ||
+    (spaces[1] == turn && spaces[4] == turn && spaces[7] == turn) ||
+    (spaces[2] == turn && spaces[5] == turn && spaces[8] == turn) ||
+    (spaces[0] == turn && spaces[4] == turn && spaces[8] == turn) ||
+    (spaces[2] == turn && spaces[4] == turn && spaces[6] == turn)
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function restart() {
+  currentPlayer = Math.floor(Math.random() * 2) ? X_TURN : O_TURN;
+  heading.innerHTML = `Turn = ${currentPlayer}`;
+  boxes.forEach((box) => {
+    box.style.backgroundColor = "";
+    box.innerHTML = null;
+  });
+  spaces.fill(null);
+
+  counts_played = 0;
+}
+
+startGame();
